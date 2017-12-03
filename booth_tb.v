ce1e07f4576927a49362680c92b7ef453408369a
RichardMuri <rmuri@umassd.edu>
2017-12-03 16:26:15 -0500

    Made some changes I don't know

--- booth_tb.v
+++ booth_tb.v
@@ -1,7 +1,8 @@
+//From figure 11.80 on p. 482
 module sel_encoder (input logic [2:0] X,
 					input logic [31:0] Y,
-					output logic [32:0] PP,
-					output logic [2:0] sel);
+					output logic [63:0] PP
+					);
 			
 			logic [2:0] sel1;
 			assign sel = sel1;
@@ -41,29 +42,30 @@ endmodule
 
 module selector (input logic [31:0] Y,
 				 input logic [2:0] sel,
-				 output logic [32:0] PP);
+				 output logic [63:0] PP);
 				 
 			// Booth Selector
-			logic [31:0] single, double, neg, Yshift, and1, and2, or1;
+			logic [31:0] single, double, neg, Yshift, and1, and2, or1, two;
+			logic [63:0] extend_full;
 			logic extend; // Sign extension
 			
 			assign single = {32{sel[2]}};
 			assign double = {32{sel[1]}};
 			assign neg = {32{sel[0]}};
+			
 			assign extend = neg[0] ^ Y[31];
+			assign extend_full = {32{extend}};
 			
 			assign and1 = Y & single;
 			assign Yshift = Y << 1;
 			assign and2 = Yshift & double;
 			
 			assign or1 = and1 | and2;
+			assign two = (or1 ^ neg) + neg[0];
 			
-			assign PP[31:0] = or1 ^ neg;
-			/*assign PP[0] = neg;
-			assign PP[1] = 1'b0;
-			assign PP[33:2] = Y_intermediate;
-			assign PP[63:34] = extend;*/
-			assign PP[32] = extend;
+			// Inefficient to do this, but trees are hard yo
+			assign PP[63:0] = {extend_full, two};
+			
 			
 endmodule
 
@@ -71,19 +73,19 @@ endmodule
 module booth_tb();
 
 logic [31:0] Y;
-logic [2:0] X, sel;
-logic [32:0] PP;
+logic [2:0] X;
+logic [63:0] PP;
 
 sel_encoder DUT(
 				.X(X),
 				.Y(Y),
-				.PP(PP),
-				.sel(sel)
+				.PP(PP)
 				);
 
 initial begin
-	Y = 32'h0000_0001;
-	
+	//Y = 32'h0000_0001;
+	Y = 32'b0000_0000_0000_0000_0000_0000_0001_1001;
+	//X = 32'b0000_0000_0000_0000_0000_0000_0010_0111;
 	for (int i = 0; i < 8; i = i + 1) 
 		begin
 			X = i;
--- shift_multiplier.v
+++ shift_multiplier.v
@@ -6,8 +6,9 @@ module shift_mult (input logic clk, reset,
 				   output logic [4:0] count);
 		
 //reg [3:0] count;
-logic [2:0] x, x0;
-logic [63:0] PP ;//hold;
+logic [2:0] x, x0, x16;
+logic [63:0] PP;
+
 //logic enable;
 parameter zero = 5'b00000, one = 5'b00001, two = 5'b00010, three = 5'b00011, four = 5'b00100;
 parameter five = 5'b00101, six = 5'b00110, seven = 5'b00111, eight = 5'b01000;
@@ -15,6 +16,7 @@ parameter nine = 5'b01001, ten = 5'b01010, eleven = 5'b01011, twelve = 5'b01100;
 parameter thirteen = 5'b01101, fourteen = 5'b01110, fifteen = 5'b01111, sixteen = 5'b10000;
 parameter start = 5'b11111, finish = 5'b10001;
 assign x0 = {X[1:0], 1'b0};
+assign x16 = {2'b0, X[31]};
 // Booth encoder/selector
 sel_encoder pp (
 				.X(x),
@@ -41,89 +43,89 @@ always @(count)
 				end
 			zero:
 				begin
-				x = X[3:1];
+				x = x0;
 				hold = PP;
 				//enable = 1'b1; 
 				end
 			one:
 				begin
 				x = X[3:1];
-				hold = {PP[61:0], 2'b0};
+				hold = PP << 2 ;
 				end
 			two:
 				begin 
 				x = X[5:3];
-				hold = {PP[59:0], 4'b0};
+				hold = PP << 4 ;
 				end
 			three:
 				begin
 				x = X[7:5];
-				hold = {PP[57:0], 6'b0};
+				hold = PP << 6 ;
 				end
 			four:
 				begin
 				x = X[9:7];
-				hold = {PP[55:0], 8'b0};
+				hold = PP << 8 ;
 				end
 			five:
 				begin
 				x = X[11:9];
-				hold = {PP[53:0], 10'b0};
+				hold = PP << 10 ;
 				end
 			six:
 				begin
 				x = X[13:11];
-				hold = {PP[51:0], 12'b0};
+				hold = PP << 12 ;
 				end
 			seven:
 				begin
 				x = X[15:13];
-				hold = {PP[49:0], 14'b0};
+				hold = PP << 14 ;
 				end
 			eight:
 				begin
 				x = X[17:15];
-				hold = {PP[47:0], 16'b0};
+				hold = PP << 16 ;
 				end
 			nine:
 				begin
 				x = X[19:17];
-				hold = {PP[45:0], 18'b0};
+				hold = PP << 18 ;
 				end
 			ten:
 				begin
 				x = X[21:19];
-				hold = {PP[43:0], 20'b0};
+				hold = PP << 20 ;
 				end
 			eleven:
 				begin
 				x = X[23:21];
-				hold = {PP[41:0], 22'b0};
+				hold = PP << 22 ;
 				end
 			twelve:
 				begin
 				x = X[25:23];
-				hold = {PP[39:0], 24'b0};
+				hold = PP << 24  ;
 				end
 			thirteen:
 				begin
 				x = X[27:25];
-				hold = {PP[37:0], 26'b0};
+				hold = PP << 26;
 				end
 			fourteen:
 				begin
 				x = X[29:27];
-				hold = {PP[35:0], 28'b0};
+				hold = PP << 28 ;
 				end
 			fifteen:
 				begin
 				x = X[31:29];
-				hold = {PP[33:0], 30'b0};
+				hold = PP << 30 ;
 				end
 			sixteen:
 				begin
-				x = {2'b00, X[31]};
-				hold = {PP[31:0], 32'b0};
+				x = x16;
+				hold = PP << 32 ;
 				end
 			finish:
 				begin
@@ -143,7 +145,7 @@ always @(posedge clk or posedge reset)
 				start:
 					count = zero;
 				zero:
-					count = 4'b0001;
+					count = one;
 				one:
 					count = two;
 				two:
@@ -181,15 +183,13 @@ always @(posedge clk or posedge reset)
 				endcase
 	end //end always
 			
-	
-
 endmodule
 
 //From figure 11.80 on p. 482
 module sel_encoder (input logic [2:0] X,
 					input logic [31:0] Y,
-					output logic [32:0] PP,
-					output logic [2:0] sel);
+					output logic [63:0] PP
+					);
 			
 			logic [2:0] sel1;
 			assign sel = sel1;
@@ -229,29 +229,30 @@ endmodule
 
 module selector (input logic [31:0] Y,
 				 input logic [2:0] sel,
-				 output logic [32:0] PP);
+				 output logic [63:0] PP);
 				 
 			// Booth Selector
-			logic [31:0] single, double, neg, Yshift, and1, and2, or1;
+			logic [31:0] single, double, neg, Yshift, and1, and2, or1, two;
+			logic [31:0] extend_full;
 			logic extend; // Sign extension
 			
 			assign single = {32{sel[2]}};
 			assign double = {32{sel[1]}};
 			assign neg = {32{sel[0]}};
+			
 			assign extend = neg[0] ^ Y[31];
+			assign extend_full = {32{extend}};
 			
 			assign and1 = Y & single;
 			assign Yshift = Y << 1;
 			assign and2 = Yshift & double;
 			
 			assign or1 = and1 | and2;
+			assign two = (or1 ^ neg) + neg[0];
 			
-			assign PP[31:0] = or1 ^ neg;
-			/*assign PP[0] = neg;
-			assign PP[1] = 1'b0;
-			assign PP[33:2] = Y_intermediate;
-			assign PP[63:34] = extend;*/
-			assign PP[32] = extend;
+			// Inefficient to do this, but trees are hard yo
+			assign PP[63:0] = {extend_full, two};
+			
 			
 endmodule
 
@@ -259,7 +260,6 @@ module reg64 (input logic reset, clk, enable,
 			 input logic [63:0] D, 
 			 output logic [63:0] Q);
 
-logic [63:0] A;
 
 always @(posedge clk)
 	if(enable)
@@ -267,12 +267,10 @@ always @(posedge clk)
 		if(reset)
 			begin
 				Q <= 0;
-				A <= 0;
 			end
 		else
 			begin
-				Q <= (A + D);
-				A <= D;
+				Q <= Q + D;
 			end
 	end
 
@@ -304,10 +302,10 @@ module shift_tb();
 	end
 	
 	initial begin
-		enable = 1'b0;
+		enable = 1'b1;
 		rst = 1'b1;
-		Y = 32'h0000_0010;
-		X = 32'h0000_0010;
+		Y = 32'b0000_0000_0000_0000_0000_0000_0001_1001;
+		X = 32'b0000_0000_0000_0000_0000_0000_0010_0111;
 		#100 enable = 1'b1; rst = 1'b0;
 		#3000 $finish;
 	end
